947a56ddb08191a9d789f07165b901fb30c4f3d5
Chad Strawinski <codesourcerer@gmail.com>
2022-12-09 21:00:09 -0600

    Initial commit

---
+++ CodeSourcerer.Library.DateTime/DateTimeExtensions.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CodeSourcerer.Library.DateTime;
+
+public static class DateTimeExtensions
+{
+    public static System.DateTime January(this int day, int year) => new System.DateTime(year, 1, day);
+}
